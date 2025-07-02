0985202729a041f3146eda2055de6a937d9b9a90
lantson <lennert.antson@kuvaspace.com>
2025-07-02 14:25:53 +0000

    refactor(tests): migration to rasterio

--- kuva-reader/tests/test_image.py
+++ kuva-reader/tests/test_image.py
@@ -1,20 +1,15 @@
 """Tests for the image processing tools."""
 
 import numpy as np
-import xarray
 
 from kuva_reader import image_to_original_range, image_to_uint16_range
 
 
 def test_normalisation():
-    """Check that normalisation function works back and forth.
-    Also that xarray information stays intact.
-    """
+    """Check that normalisation function works back and forth."""
 
     img_size = 100
-    data = np.random.uniform(0, 10000, size=[img_size, img_size])  # A 2x3 array
-    coords = {"y": np.linspace(0, 10, img_size), "x": np.linspace(0, 10, img_size)}
-    img = xarray.DataArray(data, coords=coords, dims=["y", "x"])
+    img = np.random.uniform(0, 10000, size=[img_size, img_size])  # A 2x3 array
 
     # Usual uint16 conversion
     normalised, offset, scale = image_to_uint16_range(img)
@@ -23,5 +18,3 @@ def test_normalisation():
     # Check that conversion back to original dtype works as intended
     back_to_img = image_to_original_range(normalised, offset, scale) + 1
     assert np.allclose(img, back_to_img, atol=1)
-
-    assert len(back_to_img.coords["y"]) == img_size
